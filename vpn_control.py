ede49cffb3265ad26eb22f4978c1cf21f893ecfd
shashank-2102 <shashank.prabhu21@gmail.com>
2024-06-01 00:58:51 +0200

    Added vpn functionality

---
+++ vpn_control.py
@@ -0,0 +1,21 @@
+import os
+
+def windscribe(action, location=None):
+    windscribe_cli_path = r"C:\\Program Files\\Windscribe\\windscribe-cli.exe"
+    
+    if location is None:
+        command = f'"{windscribe_cli_path}" {action}'
+    else:
+        command = f'"{windscribe_cli_path}" {action} {location}'
+    
+    os.system(command)
+
+# Connect
+windscribe("connect", "US Central")
+
+# Disconnect
+windscribe("disconnect")
+
+
+#############################        CODE FROM       #############################
+#### https://www.listendata.com/2023/08/python-code-to-run-windscribe-vpn.html ####
