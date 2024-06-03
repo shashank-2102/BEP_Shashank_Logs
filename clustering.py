b9b4a65c6c3b352896b75c078c1c0cc9b9d0fade
shashank-2102 <shashank.prabhu21@gmail.com>
2024-06-03 19:57:51 +0200

    Also returns log templates

--- clustering.py
+++ clustering.py
@@ -6,22 +6,25 @@ import numpy as np
 
 # slecting representative samples
 def generate_logs(df, tfidf_matrix, mean_shift, labels, num_representative=3):
-    representative_logs = []
+    representative_logs = {}
     for cluster in np.unique(labels):
         cluster_points = df[df['cluster'] == cluster]
         cluster_center = mean_shift.cluster_centers_[cluster]
         # distance of log to its cluster center
         distances = np.linalg.norm(tfidf_matrix[cluster_points.index].toarray() - cluster_center, axis=1)
-        # indicies of closest logs (most representative of cluster)
+        # indices of closest logs (most representative of cluster)
         closest_indices = np.argsort(distances)
         step = max(1, len(closest_indices) // num_representative)
         selected_indices = closest_indices[::step][:num_representative]
         # append most representative logs for each cluster
         for idx in selected_indices:
-            representative_logs.append(cluster_points.iloc[idx]['Log'])
+            log = cluster_points.iloc[idx]['Log']
+            template = cluster_points.iloc[idx]['LogTemplate']
+            if template not in representative_logs:
+                representative_logs[template] = ''
+            representative_logs[template] += log + '\n'  
 
-        logs_separated = '\n'.join(representative_logs)
-    return logs_separated
+    return representative_logs
 
 
 def get_representative_logs(input_path, num_representative=3):
